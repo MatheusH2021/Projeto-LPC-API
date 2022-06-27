265a2652d859ba4ddc075784b494675a30a9e6bf
MatheusH2021 <matheus.henrique.mhms@gmail.com>
2022-06-27 11:20:02 -0300

    Pequenas alterações, paginação de forma dinâmica.

--- meu-imovel/app/Http/Controllers/MeuImovelController.php
+++ meu-imovel/app/Http/Controllers/MeuImovelController.php
@@ -2,9 +2,6 @@
 
 namespace App\Http\Controllers;
 
-/*-handle-guzzle-exception-and-get-http-body-*/
-
-// use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 
